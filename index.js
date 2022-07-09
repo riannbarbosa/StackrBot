a5e2ac80381248cd011c6bdf9662b75b74afe4f9
Rian Barbosa <riannbarbosa5@gmail.com>
2022-07-09 17:35:04 -0300

    bot changes added

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const WOKCommands = require("wokcommands");
 const path = require("path");
 const { appendFile } = require("fs");
 const { Intents } = DiscordJS;
-
+var app = express();
 const host = "0.0.0.0";
 const port = process.env.PORT || 3000;
 
+app.listen(port, host, function () {
+  console.log("Server started...");
+});
 const client = new DiscordJS.Client({
   intents: [
     Intents.FLAGS.GUILDS,
@@ -16,16 +19,12 @@ const client = new DiscordJS.Client({
   ],
 });
 
-client
-  .once("ready", () => {
-    new WOKCommands(client, {
-      commandsDir: path.join(__dirname, "commands"),
-    }),
-      client.user.setActivity(`/stack`, { type: "PLAYING" });
-  })
-  .listen(port, host, function () {
-    console.log("Server started...");
-  });
+client.once("ready", () => {
+  new WOKCommands(client, {
+    commandsDir: path.join(__dirname, "commands"),
+  }),
+    client.user.setActivity(`/stack`, { type: "PLAYING" });
+});
 
 // Login to Discord with your client's token
 client.login(token);
